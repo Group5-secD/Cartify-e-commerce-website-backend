175b04b44e83388bfe1707c74babc130143e3d86
YeisraelD <Yeisraeld@gmail.com>
2026-04-13 22:34:25 +0300

    initialized Phase 2 by creating the directory structure and placeholder files

---
+++ api/addresses/manage.php
@@ -0,0 +1,4 @@
+<?php
+// Issue #14: Shipping Address Management API
+// This will handle adding, editing, and deleting user addresses.
+?>
---
+++ api/products/get_all.php
@@ -0,0 +1,4 @@
+<?php
+// Issue #10: Product Catalog API
+// This will fetch all products from the database.
+?>
